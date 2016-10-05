30861e821be8a80011fb7a052ff3b94273c96b17
Aurélien Perrier <admin@MP-AP.local>
2016-10-06 00:22:03 +0200

    first

---
+++ server/app/controllers/tools.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    tool_node: function(req, res, next)
+    {
+
+        res.json({"error" : false, "data" : output });
+    },
+
+    node_mysql: function (req, res, next)
+    {
+
+        res.json({"error" : false, "data" : password });
+    }
+};
