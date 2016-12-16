04db0598499d1b4dd35c7171ea88cbede04deb54
Aurélien Perrier <a.perrier89@gmail.com>
2016-12-16 23:34:38 +0100

    + Correction error

--- app/controllers/error.js
+++ app/controllers/error.js
@@ -6,6 +6,7 @@ module.exports = {
         {
             case 400:
                 res.status(400).send({ error: true, message: "Bad Request" });
+                //console.log(colors.error('GET > ' + req.url + ' - 400 - auth:'), req.session.username);
                 break;
 
             case 401:
