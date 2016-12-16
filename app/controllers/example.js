74ba1f51db86cf31b0d633d0fae325c6963a43c4
Aurélien Perrier <a.perrier89@gmail.com>
2016-12-17 00:33:46 +0100

    + add testId

--- app/controllers/example.js
+++ app/controllers/example.js
@@ -5,6 +5,11 @@ module.exports = {
         res.status(200).send({ error: false, message: "TEST API OK" });
     },
 
+    testId: function(req, res)
+    {
+        res.status(200).send({ error: false, message: "TEST API " + req.params.num });
+    },
+
     node_mysql: function (req, res)
     {
         res.status(200).send({ error: false, message: "API OK" });
