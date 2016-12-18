e11edb31e18b250efcd3e0afad4f2d218cfe2404
Aurélien Perrier <a.perrier89@gmail.com>
2016-12-18 01:49:28 +0100

    + Modify function error

--- app/controllers/error.js
+++ app/controllers/error.js
@@ -2,19 +2,8 @@ module.exports = {
 
     http_error: function(req, res, info)
     {
-        console.log(res.status)
         switch (info.code)
         {
-            case 201:
-                res.status(201).send({ error: false, message: info.message });
-                //console.log(colors.error('GET > ' + req.url + ' - 400 - auth:'), req.session.username);
-                break;
-
-            case 400:
-                res.status(400).send({ error: true, message: "Bad Request" });
-                //console.log(colors.error('GET > ' + req.url + ' - 400 - auth:'), req.session.username);
-                break;
-
             case 401:
                 res.status(401).send({ error: true, message: "Unauthorized" });
                 break;
@@ -33,6 +22,21 @@ module.exports = {
 
             case 500:
                 res.status(500).send({ error: true, message: "Internal Server Error" });
+                break;
+
+            default:
+                res.status(400).send({ error: true, message: "Bad Request" });
+                //console.log(colors.error('GET > ' + req.url + ' - 400 - auth:'), req.session.username);
+        }
+    },
+
+    http_success: function(req, res, info)
+    {
+        switch (info.code)
+        {
+            case 201:
+                res.status(201).send({ error: false, message: info.message });
+                //console.log(colors.error('GET > ' + req.url + ' - 400 - auth:'), req.session.username);
                 break;
 
             default:
--- app/controllers/example.js
+++ app/controllers/example.js
@@ -4,16 +4,16 @@ module.exports = {
 
     test: function(req, res)
     {
-        error.http_error(req, res, { code: 200, message: "TEST API OK" });
+        error.http_success(req, res, { code: 200, message: "TEST API OK" });
     },
 
     testId: function(req, res)
     {
-        error.http_error(req, res, { error: 200, message: "TEST API " + req.params.num });
+        error.http_success(req, res, { error: 200, message: "TEST API " + req.params.num });
     },
 
     node_mysql: function (req, res)
     {
-        error.http_error(req, res, { error: 200, message: "API OK" });
+        error.http_success(req, res, { error: 200, message: "API OK" });
     }
 };
