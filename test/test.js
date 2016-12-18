1540a1610c11662928d693aff1565dafd9f5f05a
Aurélien Perrier <a.perrier89@gmail.com>
2016-12-18 02:20:09 +0100

    + Modify test

--- test/test.js
+++ test/test.js
@@ -1,24 +1,30 @@
-var server = require("../server.js");
-var request    = require("request");
-var assert     = require('assert');
-var base_url   = "http://localhost:8080/";
+var server   = require("../server.js");
+var request  = require("request");
+var assert   = require('assert');
+var base_url = "http://localhost:8080/";
 
-describe("Test API", function() {
-    describe("ALL GET", function() {
-        it("GET /", function(done) {
-            request.get(base_url, function(error, response, body) {
-                //expect(response.statusCode).toBe(200);
-                assert.equal(200, response.statusCode);
-                done();
-            });
+describe("Test Server API", function() {
+    it("GET /", function(done) {
+        request.get(base_url, function(error, response, body) {
+            //expect(response.statusCode).toBe(200);
+            assert.equal(200, response.statusCode);
+            done();
         });
+    });
 
-        it("GET /example", function(done) {
-            request.get(base_url + "example", function(error, response, body) {
-                //expect(body).toBe("Hello World");
-                assert.equal(200, response.statusCode);
-                done();
-            });
+    it("GET /example", function(done) {
+        request.get(base_url + "example", function(error, response, body) {
+            //expect(body).toBe("Hello World");
+            assert.equal(200, response.statusCode);
+            done();
+        });
+    });
+
+    it("GET /example/1", function(done) {
+        request.get(base_url + "example/1", function(error, response, body) {
+            //expect(body).toBe("Hello World");
+            assert.equal(200, response.statusCode);
+            done();
         });
     });
 });
