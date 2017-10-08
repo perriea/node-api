7da62d9dfd64e9b50cbbd081bf38324c73c902c0
Aurélien Perrier <a.perrier89@gmail.com>
2017-10-08 15:23:43 +0200

    Updating output error & env var

--- app/services/redis.js
+++ app/services/redis.js
@@ -1,18 +1,18 @@
 // Redis client
 var redis = require('redis');
-var colors = require('./color');
+var colors = require('color');
 
 var config_redis = {
-    host: "127.0.0.1",
-    port: "6379",
+    host: process.env.APP_REDIS_HOST || '127.0.0.1',
+    port: process.env.APP_REDIS_PORT || 6379,
     retry_strategy: function (options) {
         if (options.error && options.error.code === 'ECONNREFUSED') {
             // End reconnecting on a specific error and flush all commands with a individual error
-            return new Error('The server refused the connection');
+            return new Error('Redis: the server refused the connection');
         }
         if (options.total_retry_time > 1000 * 60 * 60) {
             // End reconnecting after a specific timeout and flush all commands with a individual error
-            return new Error('Retry time exhausted');
+            return new Error('Redis: Retry time exhausted');
         }
         if (options.attempt > 10) {
             // End reconnecting with built in error
@@ -28,7 +28,7 @@ var redis_client = redis.createClient(config_redis);
 
 // Redis init client
 redis_client.on('connect', function() {
-    console.log(colors.info('\nRedis connection has been established successfully.'));
+    console.log(colors.info('\nRedis: connection has been established successfully.'));
 });
 
 module.exports = redis_client;
